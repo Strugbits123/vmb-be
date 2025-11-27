9c028642bb93337aa7be3daebc0565e3f5dbf3b1
Strugbits <145552824+Strugbits123@users.noreply.github.com>
2025-11-27 15:17:20 +0500

    updated

--- models/Admin.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const User = require("./User");
-
-const adminSchema = new mongoose.Schema({
-  fullName: { type: String, required: true },
-});
-
-module.exports = User.discriminator("admin", adminSchema);
